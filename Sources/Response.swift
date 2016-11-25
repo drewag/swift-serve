e331ad747e6fc7cef1d6cc2aa1df34e762fc9b46
Andrew J Wagner <andrew@drewagllc.com>
2016-11-25 13:49:36 -0700

    temp

--- Sources/Response.swift
+++ Sources/Response.swift
@@ -1,27 +1,37 @@
 import SwiftPlusPlus
 import Foundation
+import TextTransformers
 
 public protocol Response: CustomStringConvertible {
     var status: HTTPStatus {get}
 }
 
 extension Request {
-    public func response(status: HTTPStatus = .ok) -> Response {
-        return self.response(withData: Data(), status: status)
+    public func response(withData data: Data, status: HTTPStatus) -> Response {
+        return self.response(withData: data, status: status, headers: [:])
     }
 
-    public func response(body: String, status: HTTPStatus = .ok) -> Response {
+    public func response(withFileAt path: String, status: HTTPStatus) throws -> Response {
+        return try self.response(withFileAt: path, status: status, headers: [:])
+    }
+
+
+    public func response(status: HTTPStatus = .ok, headers: [String:String] = [:]) -> Response {
+        return self.response(withData: Data(), status: status, headers: headers)
+    }
+
+    public func response(body: String, status: HTTPStatus = .ok, headers: [String:String] = [:]) -> Response {
         let data = body.data(using: .utf8) ?? Data()
-        return self.response(withData: data, status: status)
+        return self.response(withData: data, status: status, headers: headers)
     }
 
-    public func response(json: EncodableType, status: HTTPStatus = .ok) throws -> Response {
+    public func response(json: EncodableType, status: HTTPStatus = .ok, headers: [String:String] = [:]) throws -> Response {
         let object = NativeTypesEncoder.objectFromEncodable(json)
         let data = try JSONSerialization.data(withJSONObject: object, options: JSONSerialization.WritingOptions())
-        return self.response(withData: data, status: status)
+        return self.response(withData: data, status: status, headers: headers)
     }
 
-    public func response(json: [EncodableType], status: HTTPStatus = .ok) throws -> Response {
+    public func response(json: [EncodableType], status: HTTPStatus = .ok, headers: [String:String] = [:]) throws -> Response {
         var objectArray = [Any]()
 
         for value in json {
@@ -29,10 +39,10 @@ extension Request {
         }
 
         let data = try JSONSerialization.data(withJSONObject: objectArray, options: JSONSerialization.WritingOptions())
-        return self.response(withData: data, status: status)
+        return self.response(withData: data, status: status, headers: headers)
     }
 
-    public func response(json: [String:EncodableType], status: HTTPStatus = .ok) throws -> Response {
+    public func response(json: [String:EncodableType], status: HTTPStatus = .ok, headers: [String:String] = [:]) throws -> Response {
         var objectDict = [String:Any]()
 
         for (key, value) in json {
@@ -40,7 +50,7 @@ extension Request {
         }
 
         let data = try JSONSerialization.data(withJSONObject: objectDict, options: JSONSerialization.WritingOptions())
-        return self.response(withData: data, status: status)
+        return self.response(withData: data, status: status, headers: headers)
     }
 }
 
