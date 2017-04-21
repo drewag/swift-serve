3a4693c2b1ffe10f88e93d9a1d3b41161abf35be
Andrew J Wagner <andrew@drewagllc.com>
2017-04-21 11:21:37 -0600

    [NF] Added a datbase change for creating the bounded pseudo encrypt function

--- Sources/DatabaseChange.swift
+++ Sources/DatabaseChange.swift
@@ -164,6 +164,53 @@ public struct FieldSpec: CustomStringConvertible {
     }
 }
 
+public struct CreateBoundedPseudoEncrypt: DatabaseChange {
+    public static func callWith(value: String, max: Int) -> String {
+        return "bounded_pseudo_encrypt(\(value), \(max))"
+    }
+
+    public init() {}
+
+    public var forwardQuery: String {
+        var output = ""
+        output += "CREATE FUNCTION pseudo_encrypt_24(VALUE int) returns int AS $$\n"
+        output += "DECLARE\n"
+        output += "l1 int;\n"
+        output += "l2 int;\n"
+        output += "r1 int;\n"
+        output += "r2 int;\n"
+        output += "i int:=0;\n"
+        output += "BEGIN\n"
+        output += "l1:= (VALUE >> 12) & (4096-1);\n"
+        output += "r1:= VALUE & (4096-1);\n"
+        output += "WHILE i < 3 LOOP\n"
+        output += "l2 := r1;\n"
+        output += "r2 := l1 # ((((1366 * r1 + 150889) % 714025) / 714025.0) * (4096-1))::int;\n"
+        output += "l1 := l2;\n"
+        output += "r1 := r2;\n"
+        output += "i := i + 1;\n"
+        output += "END LOOP;\n"
+        output += "RETURN ((l1 << 12) + r1);\n"
+        output += "END;\n"
+        output += "$$ LANGUAGE plpgsql strict immutable;\n"
+
+        output += "CREATE FUNCTION bounded_pseudo_encrypt(VALUE int, MAX int) returns int AS $$\n"
+        output += "BEGIN\n"
+        output += "LOOP\n"
+        output += "VALUE := pseudo_encrypt_24(VALUE);\n"
+        output += "EXIT WHEN VALUE <= MAX;\n"
+        output += "END LOOP;\n"
+        output += "RETURN VALUE;\n"
+        output += "END\n"
+        output += "$$ LANGUAGE plpgsql strict immutable;"
+        return output
+    }
+
+    public var revertQuery: String? {
+        return "DROP FUNCTION bounded_pseudo_encrypt(int,int);DROP FUNCTION pseudo_encrypt_24(int)"
+    }
+}
+
 public struct CreateSequence: DatabaseChange {
     let name: String
 
