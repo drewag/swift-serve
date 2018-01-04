c253f7836770e9667cc1f7a28cde8f235e6f2320
Andrew Wagner <coding-git@drewag.me>
2018-01-03 22:18:17 -0700

    [BF] Fix postgres package url

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftServe",
     dependencies: [
-        .Package(url: "https://github.com/drewag/swift-postgresql", majorVersion: 0),
+        .Package(url: "https://github.com/drewag/swift-postgresql.git", majorVersion: 0),
         .Package(url: "https://github.com/drewag/text-transformers.git", majorVersion: 7),
         .Package(url: "https://github.com/drewag/command-line-parser.git", majorVersion: 2),
     ]
