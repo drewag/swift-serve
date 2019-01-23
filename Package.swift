d7bdc20fee63cb78c318dc78722f60000455d281
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2019-01-23 12:33:44 -0700

    [UP] Update to sql 2.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SwiftServe", targets: ["SwiftServe"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/drewag/swift-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/drewag/swift-postgresql.git", from: "2.0.0"),
         .package(url: "https://github.com/drewag/command-line-parser.git", from: "2.0.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.11.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Markdown.git", from: "3.0.0"),
