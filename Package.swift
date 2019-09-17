ee2dc074f1bb0541e7bc84cc417f9e52322f8b2c
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-09-16 22:11:22 -0600

    [UP] Update to Swift 5.1

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
         .library(name: "SwiftServe", targets: ["SwiftServe"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/drewag/swift-postgresql.git", from: "4.0.0"),
+        .package(url: "https://github.com/drewag/swift-postgresql.git", from: "5.0.0"),
         .package(url: "https://github.com/drewag/command-line-parser.git", from: "3.0.0"),
         .package(url: "https://github.com/drewag/Stencil.git", from: "0.11.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Markdown.git", from: "3.0.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
         .package(url: "https://github.com/drewag/Decree.git", from: "4.0.0"),
-        .package(url: "https://github.com/drewag/SwiftlierCLI.git", from: "5.0.0"),
+        .package(url: "https://github.com/drewag/SwiftlierCLI.git", from: "6.0.0"),
     ],
     targets: [
         .target(name: "SwiftServe", dependencies: [
